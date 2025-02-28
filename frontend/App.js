4745a2bdd0ae08adcc3d1c47bef35d725b7fce3a
Dougmega182 <djbbinteractive@gmail.com>
2025-02-28 16:04:42 +1100

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import UserList from './components/UserList';
+import vite from '@vite/core';
+import { Router, assets } from '@vite/router';
+import { Tag } from './components/common/tags';
 
-function App() {
+const router = Router({
+  routes: [
+    { path: '/?', defaults: { name: 'home' } },
+    { path: '/api', defaults: { action: 'api' } }
+  ],
+  tags: ['prod']
+});
+
+export default function App() {
   return (
-    <div>
-      <UserList />
-    </div>
+    <vite
+      router={router}
+      imports={['./components', './ assets/']}
+      buildOptions={{
+        html: { exclude: ['app.css'] },
+        js: { compile: true }
+      }}
+    >
+      {/* Core Vite components */}
+      <Tag name="home" />
+      <Tag action="api" />
+      
+      {/* Your frontend components here */}
+    </vite>
   );
 }
-
-export default App;
